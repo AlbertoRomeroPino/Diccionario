0097f905e925749f5ca09fbe74ad3f4e00a82742
Alberto <albertoromeropino2004@gmail.com>
2025-03-06 19:14:13 +0100

    finalizando

--- src/main/java/org/example/diccionario/repositories/DefinicionRepository.java
+++ src/main/java/org/example/diccionario/repositories/DefinicionRepository.java
@@ -3,9 +3,11 @@ package org.example.diccionario.repositories;
 import org.example.diccionario.models.Definicion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface DefinicionRepository extends JpaRepository<Definicion, Long> {
     @Query(
             value = "SELECT * FROM definicion AS de WHERE de.palabra_id = ?1",
